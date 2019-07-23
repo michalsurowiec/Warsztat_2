cf262cfe2332aadcba2c16b908f16a0d0c2001f6
michalsurowiec <michalsurowiec94@o2.pl>
2019-07-23 20:39:06 +0200

    fixed logging for user

--- src/pl/coderslab/main/Main.java
+++ src/pl/coderslab/main/Main.java
@@ -1,5 +1,6 @@
 package pl.coderslab.main;
 
+import org.mindrot.jbcrypt.BCrypt;
 import pl.coderslab.dao.UserDao;
 import pl.coderslab.dao.UserGroupDao;
 import pl.coderslab.plain.User;
@@ -12,32 +13,29 @@ import java.util.Scanner;
 public class Main {
 
     /*
-
     Do zrobienia:
     - AddingSolution
-    - UserDao
     - AssignExerciseToUser:
         - przeglądanie rozwiązań które nie zostały dodane
         - przeglądanie rozwiązań dla konkretnego zadania
-
+    - UserManagement - dodanie informacji o zdolnościach kursanta
      */
+
     public static void main(String[] args) {
         boolean userIsLoggingIn = true;
         String email = "";
         while (userIsLoggingIn){
-            System.out.println("Podaj swój email lub wpisz null jeżeli chcesz zrezygnować z logowania");
+            System.out.println("Podaj swój email lub wpisz quit jeżeli chcesz zrezygnować z logowania");
             Scanner scanner = new Scanner(System.in);
             email = scanner.next();
-            if (!(email.equals("null"))) {
+            if (!(email.equals("quit"))) {
                 User user = UserDao.readByEmail(email);
-                if (user.equals(null)){
+                if (user == null){
                     System.out.println("Taki użytkownik nie istnieje! Spróbuj ponownie");
                 } else {
                     Scanner scannerTwo = new Scanner(System.in);
                     System.out.println("Wpisz hasło");
-                    User userLog = new User();
-                    userLog.hashPassword(scannerTwo.next());
-                    if (userLog.getPassword().equals(user.getPassword())){
+                    if (BCrypt.checkpw(scannerTwo.nextLine(), user.getPassword())){
                         System.out.println("Brawo! Zalogowałeś się");
                         userIsLoggingIn = false;
                     } else {
